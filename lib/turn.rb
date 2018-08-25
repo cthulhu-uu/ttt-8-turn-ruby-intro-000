52f9b2aafacdf7971ec7aafebf1dcbeac6029f0a
Sky Resendez <sky.resendez@gmail.com>
2018-08-25 22:22:25 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def turn
+
+end
+
+display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-" * 11
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-" * 11
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+end
+
+valid_move
+
+end
+
+move
+
+end
